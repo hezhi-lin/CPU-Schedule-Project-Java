df24cb632961fd3a082eff3fe502e859358dc8af
hezhi-lin <hezhi.lin.august@gmail.com>
2014-11-19 14:47:52 -0600

    Update CPUSchedule.java

--- CMPS500 Project2 Version1/src/CPUSchedule.java
+++ CMPS500 Project2 Version1/src/CPUSchedule.java
@@ -7,8 +7,6 @@ import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 //sds
-//test
-//test2
 public class CPUSchedule {
 
 	public static void main(String[] args) throws Exception {
